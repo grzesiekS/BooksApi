b3949c0b1468f180d5638a8e75ff3ea5187af09c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-23 10:44:37 +0200

    Test if fullName is string

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -2,5 +2,12 @@ const formatFullname = require('../formatFullname.js');
 const expect = require('chai').expect;
 
 describe('FormatFullname', () => {
-    
+    it('should throw Error if "fullName" is not a string', () => {
+        expect(formatFullname()).to.equal('Error');
+        expect(formatFullname(22)).to.equal('Error');
+        expect(formatFullname([])).to.equal('Error');
+        expect(formatFullname({})).to.equal('Error');
+        expect(formatFullname(undefined)).to.equal('Error');
+        expect(formatFullname(function() {})).to.equal('Error');
+    });
 });

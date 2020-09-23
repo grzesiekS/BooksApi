3476db83a58104cf90d01b738db91c636ef151ba
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-23 10:21:25 +0200

    Add first tests for cutText function

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const cutText = require('../cutText.js');
+
+describe('CutText', () => {
+    it('should return an error if "content" arg is not a string', () => {
+        expect(cutText(undefined, 20)).to.equal('Error');
+        expect(cutText(12, 20)).to.equal('Error');
+        expect(cutText({}, 20)).to.equal('Error');
+        expect(cutText([], 20)).to.equal('Error');
+        expect(cutText(function() {}, 20)).to.equal('Error');
+    });
+});
